c371a251beb8822395c0ceb7eab5fae2ecd4ae27
cojeagabriel <cojeagabriel@yahoo.com>
2019-06-20 12:46:45 +0300

    routes folder and mongodb config added

---
+++ src/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'secret': 'banana',
+    'database': 'mongodb://admin:2bornot2b@ds341247.mlab.com:41247/inventhory'
+};
--- src/server.js
+++ src/server.js
@@ -6,92 +6,21 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser'); // middleware, req.body
 const cors = require('cors'); // cross origin resource sharing
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
-let contacts = require('./data');
+var config = require('./config');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
 
-const router = express.Router();
+mongoose.connect(config.database);
+app.set('superSecret', config.secret);
+app.use(morgan('dev'));
 
-router.get('/', (req, res)=>{
-    res.json(contacts);
-});
 
-router.get('/:id', (req, res)=>{
-
-    let id = req.params.id;
-
-    let contact = contacts.filter(contact => {
-        return contact.id == id
-    })[0];
-    
-    if(!contact){
-        res.status(404).json({message: 'No contact found with this id'});
-    }
-
-    res.json(contact);
-});
-
-router.post('/', (req, res)=>{
-    let contact = {
-        id: contacts.length + 1,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        website: req.body.website
-    }
-
-    contacts.push(contact);
-
-    res.json(contact);
-});
-
-router.put('/:id', (req, res)=>{
-    let id = req.params.id;
-    let contact = contacts.filter(contact => {
-        return contact.id == id
-    })[0];
-
-    if (!contact) {
-        res.status(404).json({
-            message: 'No contact found with this id'
-        });
-    }
-
-    let index = contacts.indexOf(contact);
-
-    let keys = Object.keys(req.body);
-
-    keys.forEach(key => {
-        contact[key] = req.body[key];
-    });
-
-    contacts[index] = contact;
-
-    res.json(contacts[index]);
-});
-
-router.delete('/:id', (req, res) => {
-    let id = req.params.id;
-    let contact = contacts.filter(contact => {
-        return contact.id == id
-    })[0];
-
-    if (!contact) {
-        res.status(404).json({
-            message: 'No contact found with this id'
-        });
-    }
-    let index = contacts.indexOf(contact);
-
-    contacts.splice(index, 1);
-    res.json({message: 'User with ' + id + ' was deleted'});
-});
-
-app.use('/.netlify/functions/server', router);
-
-// app.listen(port);
+const items = require('./routes/items');
+app.use('/.netlify/functions/server/api/items', items);
 
 module.exports.handler = serverless(app);
